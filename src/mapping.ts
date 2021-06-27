45fb59c129c83e21f13c0cf7251f7315813eec78
Anton Yip <anton_yip@hotmail.com>
2021-06-27 02:13:29 -0700

    Initial commit

---
+++ src/mapping.ts
@@ -0,0 +1,62 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  EtherDelta1,
+  Order,
+  Trade,
+  Deposit,
+  Withdraw
+} from "../generated/EtherDelta1/EtherDelta1"
+import { ExampleEntity } from "../generated/schema"
+
+export function handleOrder(event: Order): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = ExampleEntity.load(event.transaction.from.toHex())
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (entity == null) {
+    entity = new ExampleEntity(event.transaction.from.toHex())
+
+    // Entity fields can be set using simple assignments
+    entity.count = BigInt.fromI32(0)
+  }
+
+  // BigInt and BigDecimal math are supported
+  entity.count = entity.count + BigInt.fromI32(1)
+
+  // Entity fields can be set based on event parameters
+  entity.tokenGet = event.params.tokenGet
+  entity.amountGet = event.params.amountGet
+
+  // Entities can be written to the store with `.save()`
+  entity.save()
+
+  // Note: If a handler doesn't require existing field values, it is faster
+  // _not_ to load the entity from the store. Instead, create it fresh with
+  // `new Entity(...)`, set the fields that should be updated and save the
+  // entity back to the store. Fields that were not set or unset remain
+  // unchanged, allowing for partial updates to be applied.
+
+  // It is also possible to access smart contracts from mappings. For
+  // example, the contract that has emitted the event can be connected to
+  // with:
+  //
+  // let contract = Contract.bind(event.address)
+  //
+  // The following functions can then be called on this contract to access
+  // state variables and other data:
+  //
+  // - contract.feeMake(...)
+  // - contract.feeAccount(...)
+  // - contract.testTrade(...)
+  // - contract.feeTake(...)
+  // - contract.balanceOf(...)
+  // - contract.availableVolume(...)
+}
+
+export function handleTrade(event: Trade): void {}
+
+export function handleDeposit(event: Deposit): void {}
+
+export function handleWithdraw(event: Withdraw): void {}
